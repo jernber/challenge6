6ee04d3b025932f414d64c63b70a7d6bc496246d
jernber <oraclemars@gmail.com>
2020-03-20 13:21:54 -0500

    fixed type in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get 'customers/index'
-  get 'customers/alphabetical'
+  get 'customers/alphabetized'
   get 'customers/missing_email'
   root to: 'customers#index'
 
